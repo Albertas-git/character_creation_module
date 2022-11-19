3cc596845690500f54ca0d3f85684a89f48b83f0
Альбертас Пятронайтис <albert-p2009@yandex.ru>
2022-11-19 16:20:07 +0300

    in code add annotation

---
+++ test code.py
@@ -0,0 +1,12 @@
+name = "Eric"
+profession = "comedian"
+affiliation = "Monty Python"
+ 
+
+message = f"""
+    Hi {name}. 
+    You are a {profession}. 
+    You were in {affiliation}.
+"""
+ 
+print(message)
